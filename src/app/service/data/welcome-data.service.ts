f60a21718820370b62c1dad28cafd9ee7396853c
OQMKa <omkarnake16@gmail.com>
2025-10-18 21:19:36 +0530

    GET/PUT/POST/DELETE/ 1.GET --> backend is working, started on frontend

--- src/app/service/data/welcome-data.service.ts
+++ src/app/service/data/welcome-data.service.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+export class HelloWorldBean {
+  constructor(public message:string){
+
+  }
+}
 @Injectable({
   providedIn: 'root'
 })
 export class WelcomeDataService {
   constructor(private httpClient:HttpClient) { }
-  executeHelloWorldBeanService(){
-    // return this.httpClient.get('http://localhost:8080/hello-world-bean/users/Saima');
-    console.log("execute Hello World bean Service");
+  executeHelloWorldBeanService(): Observable<HelloWorldBean> {
+    return this.httpClient.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean`);
+    // console.log("execute Hello World bean Service");
+  }
+
+  executeHelloWorldBeanServiceWithPath(name: string): Observable<HelloWorldBean> {
+  return this.httpClient.get<HelloWorldBean>(`http://localhost:8080/hello-world-bean/users/${name}`);
+    // console.log("execute Hello World bean Service");
   }
   
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {  WelcomeDataService } from '../service/data/welcome-data.service';
+import { HelloWorldBean, WelcomeDataService } from '../service/data/welcome-data.service';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent {
-constructor(private router: Router,
+export class WelcomeComponent implements OnInit {
+  name : string = 'om';   
+  constructor(private router: Router,
     private service: WelcomeDataService
   ) {
 
   }
-  getWelcomeMessage() {
-    // this.welcomeDataService.executeHelloWorldBeanService()-.subscribe();
-    this.service.executeHelloWorldBeanService();
+  welcomeMessageFromService: string = 'Customized welcome message';
+  errorMessageFromService: string = 'Error Message';
+  
+  ngOnInit(): void {
+    // this.getWelcomeMessage()
+  }
+  
+  getWelcomeMessageWithParameter() {
+    this.service.executeHelloWorldBeanServiceWithPath(this.name).subscribe({
+      next:response => this.handleSuccessfulResponse(response),
+      error:error => this.handleErrorResponse(error)
+  });
+  }
+  handleSuccessfulResponse(response: any) {
+    this.welcomeMessageFromService = response.message;
+  }
+  handleErrorResponse(error: any) {
+    this.errorMessageFromService = error.error.message;
+    console.log(error.error.messageror);
   }
 
   gotoToDosManager() {
